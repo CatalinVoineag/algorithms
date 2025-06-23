5a32083c49f61825144f61a2d1156a4a61f089d0
CatalinVoineag <11318084+CatalinVoineag@users.noreply.github.com>
2025-06-23 20:00:20 +0100

    Indexed Min heap with update and working with hashes

--- min_heap.rb
+++ min_heap.rb
@@ -1,31 +1,64 @@
+# frozen_string_literal: true
+
 require 'byebug'
 
 class MinHeap
-  attr_accessor :data
+  attr_accessor :data, :index_hash
 
   def initialize
     @data = []
+    @index_hash = {}
   end
 
-  def insert(value)
-    data << value
+  def insert(hash)
+    # {key: 'A', value: 1}
+    data << hash
     heapify_up(index: data.size - 1)
-    value
+    hash
   end
 
   def delete
-    return if data.size.zero?
+    return if data.empty?
 
     # Move last to first
     first_element = data.first
     last_element = data.pop
 
-    if !data.size.zero?
+    unless data.empty?
       data[0] = last_element
+
+      # this is needed when right index is nil and we don't swap in heapify_down
+      index_hash[0] = last_element if data.size <= 3
+
       heapify_down(index: 0)
     end
 
+    # delete last element in hash, this should be an orphan element
+    index_hash.delete(data.size)
+
     first_element
+  end
+
+  def update(index:, new_hash:)
+    raise "Index #{index} is out of bounds" if index >= data.size
+
+    #old_hash = data[index]
+    old_value = data[index].fetch(:value)
+    new_value = new_hash.fetch(:value)
+
+    return if new_value == old_value
+
+    data[index] = new_hash
+
+    if new_value > old_value
+      #heapify_down(index: 0)
+      heapify_down(index:)
+
+      # if new value remains in the same position update the hash index
+      index_hash[index] = new_hash if data[index] == new_hash
+    else
+      heapify_up(index:)
+    end
   end
 
   private
@@ -37,33 +70,51 @@ class MinHeap
 
     smallest_index = index
 
-    if left_index < data.size && data[left_index] < data[smallest_index]
+    if left_index < data.size && data[left_index].fetch(:value) < data[smallest_index].fetch(:value)
       smallest_index = left_index
     end
 
-    if right_index < data.size && data[right_index] < data[smallest_index]
+    if right_index < data.size && data[right_index].fetch(:value) < data[smallest_index].fetch(:value)
       smallest_index = right_index
     end
 
-    if smallest_index != index
-      data[index], data[smallest_index] = data[smallest_index], data[index]
-      heapify_down(index: smallest_index)
-    end
+    return unless smallest_index != index
+
+    data[index], data[smallest_index] = data[smallest_index], data[index]
+
+    index_hash[index] = data[index]
+    index_hash[smallest_index] = data[smallest_index]
+
+    heapify_down(index: smallest_index)
   end
 
   def heapify_up(index:)
-    return if index.zero?
+    if index.zero?
+      index_hash[index] = data[index]
+      return
+    end
 
     parent_index = parent(index:)
-    parent_value = data[parent_index]
-    value = data[index]
+    parent_value = data[parent_index].fetch(:value)
+    current_value = data[index].fetch(:value)
 
-    return if parent_value <= value
+    if parent_value <= current_value
+      index_hash[index] = data[index] unless index_hash.key?(index)
+      return
+    end
 
     # swap our value with our parent
     # Swap heapify_up until parent_value <= value
-    data[index] = parent_value
-    data[parent_index] = value
+    parent_hash = data[parent_index]
+    current_value_hash = data[index]
+
+    data[index] = parent_hash
+    data[parent_index] = current_value_hash
+
+    # swap indexes in hash
+    index_hash[parent_index] = current_value_hash
+    index_hash[index] = parent_hash
+
     heapify_up(index: parent_index)
   end
 
--- spec/min_heap_spec.rb
+++ spec/min_heap_spec.rb
@@ -1,44 +1,83 @@
-require "rspec"
-require_relative "../min_heap"
+# frozen_string_literal: true
+
+require 'rspec'
+require_relative '../min_heap'
 
 RSpec.describe MinHeap do
   subject(:heap) { described_class.new }
 
-  describe "insert and delete" do
-    it "inserts and deletes correctly" do
-      heap.insert(5)
-      heap.insert(7)
-      heap.insert(3)
-      heap.insert(10)
-      heap.insert(8)
+  describe 'insert and delete' do
+    it 'inserts and deletes correctly' do
+      heap.insert({ key: 'A', value: 5 })
+      heap.insert({ key: 'B', value: 7 })
+      heap.insert({ key: 'C', value: 3 })
+      heap.insert({ key: 'D', value: 10 })
+      heap.insert({ key: 'E', value: 8 })
 
-      expect(heap.data).to eq([3, 7, 5, 10, 8])
-
-      heap.delete
-      expect(heap.data).to eq([5, 7, 8, 10])
-      heap.delete
-      expect(heap.data).to eq([7, 10, 8])
-      heap.delete
-      expect(heap.data).to eq([8, 10])
-
-      heap.insert(1)
-
-      expect(heap.data).to eq([1, 10, 8])
+      expect(heap.data).to eq(
+        [
+          { key: 'C', value: 3 },
+          { key: 'B', value: 7 },
+          { key: 'A', value: 5 },
+          { key: 'D', value: 10 },
+          { key: 'E', value: 8 }
+        ]
+      )
 
       heap.delete
-      expect(heap.data).to eq([8, 10])
+      expect(heap.data).to eq(
+        [
+          { key: 'A', value: 5 },
+          { key: 'B', value: 7 },
+          { key: 'E', value: 8 },
+          { key: 'D', value: 10 }
+        ]
+      )
       heap.delete
-      expect(heap.data).to eq([10])
+      expect(heap.data).to eq(
+        [
+          { key: 'B', value: 7 },
+          { key: 'D', value: 10 },
+          { key: 'E', value: 8 }
+        ]
+      )
+      heap.delete
+      expect(heap.data).to eq(
+        [
+          { key: 'E', value: 8 },
+          { key: 'D', value: 10 }
+        ]
+      )
+
+      heap.insert({ key: 'A', value: 1 })
+
+      expect(heap.data).to eq(
+        [
+          { key: 'A', value: 1 },
+          { key: 'D', value: 10 },
+          { key: 'E', value: 8 }
+        ]
+      )
+
+      heap.delete
+      expect(heap.data).to eq(
+        [
+          { key: 'E', value: 8 },
+          { key: 'D', value: 10 }
+        ]
+      )
+      heap.delete
+      expect(heap.data).to eq([{ key: 'D', value: 10 }])
       heap.delete
       expect(heap.data).to eq([])
 
-      heap.insert(100)
-      heap.insert(1)
-      heap.insert(1)
-      heap.insert(1)
-      heap.insert(1)
-      heap.insert(1)
-      heap.insert(1)
+      heap.insert({ key: 'A', value: 100 })
+      heap.insert({ key: 'B', value: 1 })
+      heap.insert({ key: 'C', value: 1 })
+      heap.insert({ key: 'D', value: 1 })
+      heap.insert({ key: 'E', value: 1 })
+      heap.insert({ key: 'F', value: 1 })
+      heap.insert({ key: 'G', value: 1 })
       heap.delete
       heap.delete
       heap.delete
@@ -50,20 +89,385 @@ RSpec.describe MinHeap do
     end
   end
 
-  describe "test edge case" do
-    it "insert and deletes correctly" do
-      heap.insert(5)
-      heap.insert(10)
-      heap.insert(11)
-      heap.insert(7)
+  describe 'test edge case' do
+    it 'insert and deletes correctly' do
+      heap.insert({ key: 'A', value: 5 })
+      heap.insert({ key: 'B', value: 10 })
+      heap.insert({ key: 'C', value: 11 })
+      heap.insert({ key: 'D', value: 7 })
 
-      expect(heap.data).to eq([5, 7, 11, 10])
+      expect(heap.data).to eq(
+        [
+          { key: 'A', value: 5 },
+          { key: 'D', value: 7 },
+          { key: 'C', value: 11 },
+          { key: 'B', value: 10 }
+        ]
+      )
       heap.delete
-      expect(heap.data).to eq([7, 10, 11])
+      expect(heap.data).to eq(
+        [
+          { key: 'D', value: 7 },
+          { key: 'B', value: 10 },
+          { key: 'C', value: 11 }
+        ]
+      )
       heap.delete
-      expect(heap.data).to eq([10, 11])
+      expect(heap.data).to eq(
+        [
+          { key: 'B', value: 10 },
+          { key: 'C', value: 11 }
+        ]
+      )
       heap.delete
-      expect(heap.data).to eq([11])
+      expect(heap.data).to eq([{ key: 'C', value: 11 }])
+    end
+  end
+
+  describe 'index keeping' do
+    it 'updates the indexes correctly' do
+      heap.insert({ key: 'A', value: 50 })
+      heap.insert({ key: 'B', value: 71 })
+      heap.insert({ key: 'C', value: 100 })
+      heap.insert({ key: 'D', value: 101 })
+      heap.insert({ key: 'E', value: 80 })
+      heap.insert({ key: 'F', value: 200 })
+      heap.insert({ key: 'G', value: 105 })
+
+      expect(heap.data).to eq(
+        [
+          { key: 'A', value: 50 },
+          { key: 'B', value: 71 },
+          { key: 'C', value: 100 },
+          { key: 'D', value: 101 },
+          { key: 'E', value: 80 },
+          { key: 'F', value: 200 },
+          { key: 'G', value: 105 }
+        ]
+      )
+
+      expect(heap.index_hash).to eq(
+        {
+          0 => { key: 'A', value: 50 },
+          1 => { key: 'B', value: 71 },
+          2 => { key: 'C', value: 100 },
+          3 => { key: 'D', value: 101 },
+          4 => { key: 'E', value: 80 },
+          5 => { key: 'F', value: 200 },
+          6 => { key: 'G', value: 105 }
+        }
+      )
+
+      heap.delete
+      expect(heap.data).to eq(
+        [
+          { key: 'B', value: 71 },
+          { key: 'E', value: 80 },
+          { key: 'C', value: 100 },
+          { key: 'D', value: 101 },
+          { key: 'G', value: 105 },
+          { key: 'F', value: 200 }
+        ]
+      )
+      expect(heap.index_hash).to eq(
+        {
+          0 => { key: 'B', value: 71 },
+          2 => { key: 'C', value: 100 },
+          3 => { key: 'D', value: 101 },
+          1 => { key: 'E', value: 80 },
+          5 => { key: 'F', value: 200 },
+          4 => { key: 'G', value: 105 }
+        }
+      )
+
+      heap.delete
+      expect(heap.data).to eq(
+        [
+          { key: 'E', value: 80 },
+          { key: 'D', value: 101 },
+          { key: 'C', value: 100 },
+          { key: 'F', value: 200 },
+          { key: 'G', value: 105 }
+        ]
+      )
+      expect(heap.index_hash).to eq(
+        {
+          2 => { key: 'C', value: 100 },
+          1 => { key: 'D', value: 101 },
+          0 => { key: 'E', value: 80 },
+          3 => { key: 'F', value: 200 },
+          4 => { key: 'G', value: 105 }
+        }
+      )
+
+      heap.delete
+      expect(heap.data).to eq(
+        [
+          { key: 'C', value: 100 },
+          { key: 'D', value: 101 },
+          { key: 'G', value: 105 },
+          { key: 'F', value: 200 }
+        ]
+      )
+      expect(heap.index_hash).to eq(
+        {
+          0 => { key: 'C', value: 100 },
+          1 => { key: 'D', value: 101 },
+          3 => { key: 'F', value: 200 },
+          2 => { key: 'G', value: 105 }
+        }
+      )
+
+      heap.delete
+      expect(heap.data).to eq(
+        [
+          { key: 'D', value: 101 },
+          { key: 'F', value: 200 },
+          { key: 'G', value: 105 }
+        ]
+      )
+      expect(heap.index_hash).to eq(
+        {
+          0 => { key: 'D', value: 101 },
+          1 => { key: 'F', value: 200 },
+          2 => { key: 'G', value: 105 }
+        }
+      )
+
+      heap.delete
+      expect(heap.data).to eq(
+        [
+          { key: 'G', value: 105 },
+          { key: 'F', value: 200 }
+        ]
+      )
+      expect(heap.index_hash).to eq(
+        {
+          1 => { key: 'F', value: 200 },
+          0 => { key: 'G', value: 105 }
+        }
+      )
+
+      heap.delete
+      expect(heap.data).to eq(
+        [
+          { key: 'F', value: 200 }
+        ]
+      )
+      expect(heap.index_hash).to eq(
+        {
+          0 => { key: 'F', value: 200 }
+        }
+      )
+
+      heap.delete
+      expect(heap.data).to eq([])
+      expect(heap.index_hash).to eq({})
+    end
+  end
+
+  describe 'update' do
+    it 'updates the values correctly' do
+      heap.insert({ key: 'A', value: 50 })
+      heap.insert({ key: 'B', value: 71 })
+      heap.insert({ key: 'C', value: 100 })
+      heap.insert({ key: 'D', value: 101 })
+      heap.insert({ key: 'E', value: 80 })
+      heap.insert({ key: 'F', value: 200 })
+      heap.insert({ key: 'G', value: 105 })
+
+      expect(heap.data).to eq(
+        [
+          { key: 'A', value: 50 },
+          { key: 'B', value: 71 },
+          { key: 'C', value: 100 },
+          { key: 'D', value: 101 },
+          { key: 'E', value: 80 },
+          { key: 'F', value: 200 },
+          { key: 'G', value: 105 }
+        ]
+      )
+
+      heap.update(index: 0, new_hash: { key: 'A', value: 51 })
+      expect(heap.data).to eq(
+        [
+          { key: 'A', value: 51 },
+          { key: 'B', value: 71 },
+          { key: 'C', value: 100 },
+          { key: 'D', value: 101 },
+          { key: 'E', value: 80 },
+          { key: 'F', value: 200 },
+          { key: 'G', value: 105 }
+        ]
+      )
+      expect(heap.index_hash).to eq(
+        {
+          0 => { key: 'A', value: 51 },
+          1 => { key: 'B', value: 71 },
+          2 => { key: 'C', value: 100 },
+          3 => { key: 'D', value: 101 },
+          4 => { key: 'E', value: 80 },
+          5 => { key: 'F', value: 200 },
+          6 => { key: 'G', value: 105 }
+        }
+      )
+
+      heap.update(index: 4, new_hash: { key: 'E', value: 21 })
+      expect(heap.data).to eq(
+        [
+          { key: 'E', value: 21 },
+          { key: 'A', value: 51 },
+          { key: 'C', value: 100 },
+          { key: 'D', value: 101 },
+          { key: 'B', value: 71 },
+          { key: 'F', value: 200 },
+          { key: 'G', value: 105 }
+        ]
+      )
+      expect(heap.index_hash).to eq(
+        {
+          0 => { key: 'E', value: 21 },
+          1 => { key: 'A', value: 51 },
+          2 => { key: 'C', value: 100 },
+          3 => { key: 'D', value: 101 },
+          4 => { key: 'B', value: 71 },
+          5 => { key: 'F', value: 200 },
+          6 => { key: 'G', value: 105 }
+        }
+      )
+
+      heap.update(index: 5, new_hash: { key: 'F', value: 202 })
+      expect(heap.data).to eq(
+        [
+          { key: 'E', value: 21 },
+          { key: 'A', value: 51 },
+          { key: 'C', value: 100 },
+          { key: 'D', value: 101 },
+          { key: 'B', value: 71 },
+          { key: 'F', value: 202 },
+          { key: 'G', value: 105 }
+        ]
+      )
+      expect(heap.index_hash).to eq(
+        {
+          0 => { key: 'E', value: 21 },
+          1 => { key: 'A', value: 51 },
+          2 => { key: 'C', value: 100 },
+          3 => { key: 'D', value: 101 },
+          4 => { key: 'B', value: 71 },
+          5 => { key: 'F', value: 202 },
+          6 => { key: 'G', value: 105 }
+        }
+      )
+
+      heap.update(index: 6, new_hash: { key: 'G', value: 2 })
+      expect(heap.data).to eq(
+        [
+          { key: 'G', value: 2 },
+          { key: 'A', value: 51 },
+          { key: 'E', value: 21 },
+          { key: 'D', value: 101 },
+          { key: 'B', value: 71 },
+          { key: 'F', value: 202 },
+          { key: 'C', value: 100 }
+        ]
+      )
+      expect(heap.index_hash).to eq(
+        {
+          0 => { key: 'G', value: 2 },
+          1 => { key: 'A', value: 51 },
+          2 => { key: 'E', value: 21 },
+          3 => { key: 'D', value: 101 },
+          4 => { key: 'B', value: 71 },
+          5 => { key: 'F', value: 202 },
+          6 => { key: 'C', value: 100 }
+        }
+      )
+
+      heap.update(index: 0, new_hash: { key: 'G', value: 1 })
+      expect(heap.data).to eq(
+        [
+          { key: 'G', value: 1 },
+          { key: 'A', value: 51 },
+          { key: 'E', value: 21 },
+          { key: 'D', value: 101 },
+          { key: 'B', value: 71 },
+          { key: 'F', value: 202 },
+          { key: 'C', value: 100 }
+        ]
+      )
+      expect(heap.index_hash).to eq(
+        {
+          0 => { key: 'G', value: 1 },
+          1 => { key: 'A', value: 51 },
+          2 => { key: 'E', value: 21 },
+          3 => { key: 'D', value: 101 },
+          4 => { key: 'B', value: 71 },
+          5 => { key: 'F', value: 202 },
+          6 => { key: 'C', value: 100 }
+        }
+      )
+
+      heap.update(index: 0, new_hash: { key: 'G', value: 100 })
+      expect(heap.data).to eq(
+        [
+          { key: 'E', value: 21 },
+          { key: 'A', value: 51 },
+          { key: 'G', value: 100 },
+          { key: 'D', value: 101 },
+          { key: 'B', value: 71 },
+          { key: 'F', value: 202 },
+          { key: 'C', value: 100 }
+        ]
+      )
+      expect(heap.index_hash).to eq(
+        {
+          0 => { key: 'E', value: 21 },
+          1 => { key: 'A', value: 51 },
+          2 => { key: 'G', value: 100 },
+          3 => { key: 'D', value: 101 },
+          4 => { key: 'B', value: 71 },
+          5 => { key: 'F', value: 202 },
+          6 => { key: 'C', value: 100 }
+        }
+      )
+
+      expect { heap.update(index: 200, new_hash: { key: 'G', value: 100 }) }
+        .to raise_error(RuntimeError, 'Index 200 is out of bounds')
+    end
+
+    it 'updates edge case' do
+      heap.insert({ key: 'A', value: 10 })
+      heap.insert({ key: 'B', value: 20 })
+      heap.insert({ key: 'C', value: 30 })
+      heap.insert({ key: 'D', value: 40 })
+
+      expect(heap.data).to eq(
+        [
+          { key: 'A', value: 10 },
+          { key: 'B', value: 20 },
+          { key: 'C', value: 30 },
+          { key: 'D', value: 40 },
+        ]
+      )
+
+      heap.update(index: 1, new_hash: {key: 'B', value: 50})
+      expect(heap.data).to eq(
+        [
+          { key: 'A', value: 10 },
+          { key: 'D', value: 40 },
+          { key: 'C', value: 30 },
+          { key: 'B', value: 50 },
+        ]
+      )
+      expect(heap.index_hash).to eq(
+        {
+          0 => { key: 'A', value: 10 },
+          1 => { key: 'D', value: 40 },
+          2 => { key: 'C', value: 30 },
+          3 => { key: 'B', value: 50 },
+        }
+      )
     end
   end
 end
